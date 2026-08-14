a7b1bd570df94eef084392a13f04d2f371767c89
Matambo Sanogo <Matambo89@icloud.com>
2026-08-14 13:45:59 +0000

    Update app.py

--- app.py
+++ app.py
@@ -200,6 +200,8 @@ st.warning(
     "Cette application est un prototype académique "
     "de machine learning. Elle ne remplace pas "
     "un diagnostic médical professionnel."
+    "0 represente l'absence de paludisme sévère et 1 sa présence."
+    "pour le sex 0 représente le sexe féminin et 1 le sexe masculin."
 )
 # RECUPERATION DES VARIABLES DU MODELE
 
